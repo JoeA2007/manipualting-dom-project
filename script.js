30cf3eb40e7324b54f4731c4c2f58eae9c220418
JoeA2007 <joeamenumeg@gmail.com>
2025-07-30 23:31:37 -0400

    initial

---
+++ script.js
@@ -0,0 +1,15 @@
+let pButton = document.getElementById("p-button");
+let imageButton = document.getElementById("img-button")
+
+let pDiv = document.getElementById("p-div")
+let imgDiv = document.getElementById("img-div")
+
+
+    pButton.addEventListener("click",  function(){
+    console.log("Paragraph button clicked.");
+    })
+    
+    
+    imageButton.addEventListener("click", function(){
+    console.log("Image button clicked.");
+    });
